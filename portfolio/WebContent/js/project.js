a5afa26e50099a5e67613664c327c55ef6ba01c2
jangEunsue <masterthd56641221@gmail.com>
2022-07-14 15:23:13 +0900

    포토폴리오 홈페이지 개인 개발 코드

---
+++ portfolio/WebContent/js/project.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+//포트폴리오 상세내용 사이트로 이동
+	//도서구매 페이지
+	function opensujum() {
+		open("project/sujum.html","도서구매페이지","width=1000px");
+	}
+	//온습도 센서 및 openApi 페이지
+	function opendht11() {
+		open("project/dht11.html","온습도 추이 및 동파위험도","width=1000px");
+	}
+	//박데이터 경진대회
+	function openbigdate() {
+		open("project/bigdate.html","시간대 추이별 광고매체선정","width=1000px");
+	}
+	//ai데이터 기획 페이지
+	function openAi() {
+		open("project/ai.html","은행 적금 상품 출시를 위한 데이터 분석","width=1000px");
+	}
+	//음성인식 페이지
+	function openCrawling() {
+		open("project/Crawling.html","음성인식을 활용한 스무고개 퀴즈","width=1000px");
+	}
